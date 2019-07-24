0a28cceeac63bb89a3db3867b6d692fd8c59511f
ycc <ycc@hleast.com>
2019-07-24 09:09:49 +0800

    <fix>路由控制完善

--- src/http/axios.js
+++ src/http/axios.js
@@ -22,7 +22,9 @@ instance.interceptors.response.use(response => {
     if (error.response) {
         if (error.response.status === 402) {
             store.commit('setPermission', false)
-            router.replace('/')
+            router.replace('/');
+            /* 登出的时候刷新一下，让路由变为默认的静态路由 */
+            router.go(0)
         }
     }
     // 对响应错误做点什么
--- src/router.js
+++ src/router.js
@@ -62,6 +62,11 @@ let router = new Router({
     routes: staticRouter
 });
 
+
+
+/* 不用登陆就能进入的路由 */
+let whiteList = staticRouter.map(item => item.path)
+
 /* 导航守卫next后面的代码会继续执行,next方法带参数要非常小心，它会重复进入导航守卫。所以要有判断条件使其再进入时不执行上一次的next代码 */
 router.beforeEach((to, from, next) => {
     /* 有权限 */
@@ -71,20 +76,22 @@ router.beforeEach((to, from, next) => {
             store.commit('setRouters', asyncRouter)
             router.addRoutes(asyncRouter);
             /* addRouters后需必须写next(),否则可在地址栏中输入地址跳转，但是用push方法跳转的路由会去未add的路由里匹配 */
-            next(to.path)
+            // next(to.path)
         } else {
-            if (to.path !== '/login' || to.paht === '/') {
+            if (to.path === '/login' || to.path === '/') {
+                next(from.path)
+            } else {
                 next()
             }
         }
     }
     /* 没有权限 */
     else {
-        if (to.path === '/login' || to.path === '/') {
+        if (whiteList.includes(to.path)) {
             next()
         } else {
+            /* 页面刷新 */
             store.dispatch('checkPermission').then(() => {
-                /* 页面刷新 */
                 next(to.path)
             }).catch(() => {
                 next('/')
